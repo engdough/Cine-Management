aeebf85c04e4b75537e042a40aa8bfce61a5ce49
gilmar <gilmar500@gmail.com>
2019-09-10 06:34:14 -0300

    Estrutura inicial do projeto

---
+++ src/br/ufsc/ine5608/cineManagement/models/Usuario.java
@@ -0,0 +1,71 @@
+package br.usfc.ine5608.cineManagement;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Usuario implements Serializable {
+
+    private String nome;
+    private String cpf;
+    private Date dataNascimento;
+    private String email;
+    private String telefone;
+    private int pontos;
+
+    public Usuario(String nome, String cpf, Date dataNascimento, String email, String telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+        this.telefone = telefone;
+        this.pontos = 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
+}
