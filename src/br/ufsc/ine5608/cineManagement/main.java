7b81ff637f3e7cff16509bdd048e9d7260ca5339
gilmar <gilmar500@gmail.com>
2019-09-10 09:17:52 -0300

    Atualizado instanciamento

--- src/br/ufsc/ine5608/cineManagement/main.java
+++ src/br/ufsc/ine5608/cineManagement/main.java
@@ -3,9 +3,7 @@ package br.ufsc.ine5608.cineManagement;
 import br.ufsc.ine5608.cineManagement.controladores.ControladorPrincipal;
 
 public class main {
-    static ControladorPrincipal controladorPrincipal = new ControladorPrincipal();
-
     public static void main(String[] args) {
-        controladorPrincipal.iniciaSistema();
+        ControladorPrincipal.getInstancia().iniciaSistema();
     }
 }
