86bd952f81f98354fbd1ac2d77ee838faf5b19e3
gilmar <gilmar500@gmail.com>
2019-09-10 06:48:48 -0300

    atualizado entidade pessoa

--- src/br/ufsc/ine5608/cineManagement/models/Usuario.java
+++ src/br/ufsc/ine5608/cineManagement/models/Usuario.java
@@ -5,6 +5,8 @@ import java.util.Date;
 
 public class Usuario implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private String nome;
     private String cpf;
     private Date dataNascimento;
