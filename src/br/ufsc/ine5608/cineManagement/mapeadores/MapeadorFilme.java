149abf914555b754fc67bdd14e855dbb9025812f
gilmar <gilmar500@gmail.com>
2019-09-10 07:08:04 -0300

    Entidade filme com serializable

---
+++ src/br/ufsc/ine5608/cineManagement/mapeadores/MapeadorFilme.java
@@ -0,0 +1,76 @@
+package br.ufsc.ine5608.cineManagement.mapeadores;
+
+import br.ufsc.ine5608.cineManagement.models.Filme;
+
+import java.io.*;
+import java.util.Collection;
+import java.util.HashMap;
+
+public class MapeadorFilme {
+
+    private HashMap<String, Filme> dadosFilmes = new HashMap<>();
+    private final String filename = "filmes.dados";
+
+    public MapeadorFilme(){
+        load();
+    }
+
+    public Filme get(String codigo){
+        return dadosFilmes.get(codigo);
+    }
+
+    public void put(Filme filme){
+        dadosFilmes.put(filme.getCodigo(), filme);
+        persist();
+    }
+
+    public void persist(){
+        try {
+            FileOutputStream fileOut = new FileOutputStream(filename);
+            ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+            objOut.writeObject(dadosFilmes);
+            objOut.flush();
+            fileOut.flush();
+            objOut.close();
+            fileOut.close();
+        } catch (FileNotFoundException e){
+            System.out.println("Arquivo não encontrado");
+        } catch (IOException e){
+            System.out.println(e);
+        }
+    }
+
+    public  void load(){
+        try {
+            FileInputStream fileOut = new FileInputStream(filename);
+            ObjectInputStream objIn = new ObjectInputStream(fileOut);
+            this.dadosFilmes = (HashMap<String, Filme>) objIn.readObject();
+            objIn.close();
+            fileOut.close();
+        } catch (ClassNotFoundException e){
+            System.out.println("Arquivo inválido");
+            persist();
+        } catch (FileNotFoundException e){
+            System.out.println("Arquivo não encontrado");
+        } catch (IOException e){
+            System.out.println(e);
+        }
+    }
+
+    public void remove(Filme filme){
+        dadosFilmes.remove(filme.getCodigo());
+        persist();
+    }
+
+    public Collection<Filme> getList() {
+        return dadosFilmes.values();
+    }
+
+    public boolean listaVazia() {
+        return dadosFilmes.isEmpty();
+    }
+
+    public int tamanhoLista() {
+        return dadosFilmes.size();
+    }
+}
---
+++ src/br/ufsc/ine5608/cineManagement/models/Filme.java
@@ -0,0 +1,72 @@
+package br.ufsc.ine5608.cineManagement.models;
+
+import java.io.Serializable;
+
+public class Filme implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String codigo;
+    private String nome;
+    private String descricao;
+    private String classificacaoIndicativa;
+    private int duracaoMinutos;
+    private String genero;
+
+    public Filme(String codigo, String nome, String descricao, String classificacaoIndicativa, int duracaoMinutos, String genero) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.classificacaoIndicativa = classificacaoIndicativa;
+        this.duracaoMinutos = duracaoMinutos;
+        this.genero = genero;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getClassificacaoIndicativa() {
+        return classificacaoIndicativa;
+    }
+
+    public void setClassificacaoIndicativa(String classificacaoIndicativa) {
+        this.classificacaoIndicativa = classificacaoIndicativa;
+    }
+
+    public int getDuracaoMinutos() {
+        return duracaoMinutos;
+    }
+
+    public void setDuracaoMinutos(int duracaoMinutos) {
+        this.duracaoMinutos = duracaoMinutos;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
